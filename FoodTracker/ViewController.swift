805f433bb15bfae827abb03db59000c39a5503d0
Kevin Harris <kharris@guildsa.org>
2016-09-14 17:17:13 -0500

    Add Meal class.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -77,10 +77,5 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         
         presentViewController(imagePickerController, animated: true, completion: nil)
     }
-    
-    @IBAction func setDefaultLabelText(sender: UIButton) {
-        
-        mealNameLabel.text = "Default Text"
-    }
 }
 
