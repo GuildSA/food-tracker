0ff3c1ad1ee267cc01cd7a8698a5f3b2d906a8c7
Kevin Harris <kharris@guildsa.org>
2016-09-21 15:08:27 -0500

    Add some code to auot login a test user.

--- FoodTracker/AppDelegate.swift
+++ FoodTracker/AppDelegate.swift
@@ -11,11 +11,65 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    let VERSION_NUM = "v1"
+    let APP_ID = "F2C2DCED-C64B-3639-FFE7-1DCAD9C23C00"
+    let SECRET_KEY = "592D0F41-FE91-2925-FF75-C228378D4B00"
+
+    let EMAIL = "ios_user@gmail.com" // Doubles as User Name
+    let PASSWORD = "password"
+    
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        let backendless = Backendless.sharedInstance()
+        backendless?.initApp(APP_ID, secret:SECRET_KEY, version:VERSION_NUM)
+        backendless?.userService.setStayLoggedIn(true)
+
+        
+        // First, check if the user is already logged in. If they are, we don't need to
+        // ask them to login again.
+        let isValidUser = backendless?.userService.isValidUserToken()
+        
+        if isValidUser != nil && isValidUser != 0 {
+            
+            // The user has a valid user token so we know for sure the user is already logged!
+            print("User is already logged: \(isValidUser?.boolValue)");
+            
+        } else {
+            
+            let user: BackendlessUser = BackendlessUser()
+            user.email = EMAIL as NSString!
+            user.password = PASSWORD as NSString!
+            
+            backendless?.userService.registering( user,
+                                                 
+             response: { (user: BackendlessUser?) -> Void in
+                
+                print("User was registered: \(user?.objectId)")
+                
+                
+                backendless?.userService.login( self.EMAIL, password: self.PASSWORD,
+    
+                        response: { (user: BackendlessUser?) -> Void in
+                            print("User logged in: \(user!.objectId)")
+                        },
+        
+                       error: { (fault: Fault?) -> Void in
+                            print("User failed to login: \(fault)")
+                        }
+                    )
+                
+                },
+                                                     
+                error: { (fault: Fault?) -> Void in
+                    print("User failed to register: \(fault)")
+                }
+            )
+        }
+        
         return true
     }
 
